14b06e53b2fcc2e96152511bb62c54540b65bf4c
Tejas Chintala <tejas.chintala@gmail.com>
2020-08-06 22:22:37 +0530

    Updated the code.

--- automation.py
+++ automation.py
@@ -66,45 +66,6 @@ unclick = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]
 unclick.click()
 
 
-# ### Sending a message to Messenger
-# 
-
-# In[203]:
-
-
-#Clicking on the Messenger icon
-sleep(3)
-msg_icon = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[1]/div[2]/span/div/div[1]")
-msg_icon.click()
-
-#Searching for the person
-sleep(3)
-search = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div/label/input")
-search.click()
-
-#Enter the person name
-name = input("Enter the name: ")
-search_bar = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div[2]/label/input")
-search_bar.send_keys(name)
-
-#Opening the chatbox
-sleep(3)
-tab = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[2]/div/div/div[1]/div[2]/div/div/div[1]/div[1]/div/div/div[1]/ul/div[1]/li/div/div[1]/div/div[2]/span")
-tab.click()
-sleep(2)
-
-#Sending the message
-message = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[5]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div[2]/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div")
-message.send_keys("This message was sent using Selenium")
-sleep(3)
-send = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[5]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div[2]/form/div/div[3]/span[2]/div")
-send.click()
-
-#Closing the chatbox
-sleep(2)
-close = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[5]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[3]/span[4]/div")
-close.click()
-
 
 # ### Posting Story
 
@@ -186,6 +147,40 @@ else:
 #Saving the bio    
 sleep(2)
 send = browser.find_element_by_css_selector("#mount_0_0 > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.dp1hu0rb.cbu4d94t.j83agx80 > div > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.ihqw7lf3.cddn0xzi > div > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.d1544ag0.tw6a2znq.discj3wi.b5q2rw42.lq239pai.mysgfdmx.hddg9phg > div > div > div.j83agx80.cbu4d94t.obtkqiv7.sv5sfqaa > div > span > div > div > div.aahdfvyu.i1fnvgqd.j83agx80.bp9cbjyn > div:nth-child(2) > div.oajrlxb2.s1i5eluu.gcieejh5.bn081pho.humdl8nn.izx4hr6d.rq0escxv.nhd2j8a9.j83agx80.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.d1544ag0.qt6c0cv9.tw6a2znq.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.l9j0dhe7.abiwlrkh.p8dawk7l.beltcj47.p86d2i9g.aot14ch1.kzx2olss.cbu4d94t.taijpn5t.ni8dbmo4.stjgntxs.k4urcfbm.tv7at329")
+send.click()
+
+
+# ### Sending a message to Messenger 
+
+# In[203]:
+
+
+#Clicking on the Messenger icon
+sleep(3)
+msg_icon = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[1]/div[2]/span/div/div[1]")
+msg_icon.click()
+
+#Searching for the person
+sleep(3)
+search = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div/label/input")
+search.click()
+
+#Enter the person name
+name = input("Enter the name: ")
+search_bar = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div[2]/label/input")
+search_bar.send_keys(name)
+
+#Opening the chatbox
+sleep(3)
+tab = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div[4]/div[2]/div/div/div[1]/div[2]/div/div/div[1]/div[1]/div/div/div[1]/ul/div[1]/li/div/div[1]/div/div[2]/span")
+tab.click()
+sleep(2)
+
+#Sending the message
+message = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[5]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div[2]/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div")
+message.send_keys("This message was sent using Selenium")
+sleep(3)
+send = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[5]/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[2]/div/div[2]/form/div/div[3]/span[2]/div")
 send.click()
 
 
